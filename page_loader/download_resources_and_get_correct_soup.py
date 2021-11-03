a0561e9723d04f15af728293f424dffde0445f1a
Alexander Maksimovich <maksimovicth@mail.ru>
2021-11-03 21:04:32 +0300

    18th try

--- page_loader/download_resources_and_get_correct_soup.py
+++ page_loader/download_resources_and_get_correct_soup.py
@@ -70,6 +70,7 @@ def download_resource_and_correct_soup(path_to_dir_of_resources, dir_of_resource
             try:
                 out_file.write(
                     session.get(parsed_url.scheme + "://" + parsed_url.netloc + parsed_tag_url.path).content)
+                file_logger.info("Written with wb option")
             except Exception as e:
                 file_logger.exception("Something wrong with HTTP request for resource")
                 console_logger.critical("Something wrong with HTTP request for resource")
@@ -79,6 +80,7 @@ def download_resource_and_correct_soup(path_to_dir_of_resources, dir_of_resource
             try:
                 out_file.write(
                     requests.get(parsed_url.scheme + "://" + parsed_url.netloc + parsed_tag_url.path).text)
+                file_logger.info("Written with w option")
             except Exception as e:
                 file_logger.exception("Something wrong with HTTP request for resource")
                 console_logger.critical("Something wrong with HTTP request for resource")
